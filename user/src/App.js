2b548291e5fb5407190cae7f0b0788433ce16d32
satyam46020 <satyam46020@gmail.com>
2024-04-16 21:32:01 +0530

    basic react structure and userList file fetch reqest from api

---
+++ user/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ user/src/components/UserList.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+
+  return (
+    
+  );
+};
+
+export default UserList;
